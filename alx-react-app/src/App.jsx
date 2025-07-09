fe7a85c23f591d66bab767ba00c75d1320a38f56
Peter Gatonye <petrgatonye@gmail.com>
2025-07-09 10:55:32 +0300

    Added WelcomeMessage component and updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <WelcomeMessage/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
