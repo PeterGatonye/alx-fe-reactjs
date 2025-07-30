311aebbfe620038c7b126a12f1b80dbc25dba97c
Peter Gatonye <petrgatonye@gmail.com>
2025-07-30 13:55:54 +0300

    Fix: add Link in RecipeList and configure Router in App.jsx for ALX checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-
       <Routes>
         <Route
           path="/"
