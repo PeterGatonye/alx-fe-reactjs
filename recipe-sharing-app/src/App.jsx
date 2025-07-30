e1d653b1c4ebb5ef5327268a4b60fba82fec5c16
Peter Gatonye <petrgatonye@gmail.com>
2025-07-30 13:32:34 +0300

    Fix: wrap routes with Router for ALX checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div>
+    <Router>
       <nav>
         <Link to="/">Home</Link>
       </nav>
@@ -22,7 +22,7 @@ function App() {
         />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
