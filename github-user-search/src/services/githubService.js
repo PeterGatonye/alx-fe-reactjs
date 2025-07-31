dadd034cabb12f3102670924a49e6b282442f11f
Peter Gatonye <petrgatonye@gmail.com>
2025-07-31 08:31:43 +0300

    Fix error message text for ALX checker

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 export const fetchUserData = async (username) => {
   const url = `https://api.github.com/users/${username}`;
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(url);
+  return response.data;
 };
